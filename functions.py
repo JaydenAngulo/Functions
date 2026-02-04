cbde58f07192571f51ce73128bfc34209329c1eb
JaydenAngulo <jaydenangulo21@icloud.com>
2026-02-04 17:27:05 +0000

    good

---
+++ functions.py
@@ -0,0 +1,94 @@
+# Challenge 1:
+
+# PROCEDURE calculator(a,b)
+# {
+#  DISPLAY(a + b)
+#  DISPLAY(a - b)
+#  DISPLAY(a / b)
+#  DISPLAY(a * b)
+# }
+
+# calculator(a,b)
+
+def calculator(a,b):
+    print(a + b)
+    print(a - b)
+    print(a / b)
+    print(a * b)
+
+calculator(a,b)
+
+# Challenge 2:
+
+# PROCEDURE average_finder(a,b,c)
+# {
+# sum <- a + b + c
+# average <- sum / 3
+# RETURN(average)
+# }
+
+# average_finder(a,b,c)
+
+# DISPLAY(average)
+
+def average_finder(a,b,c):
+    sum = a + b + c
+    average = sum / 3
+    return average
+
+average_finder(a,b,c)
+
+print(average)
+
+# Challenge 3:
+
+# PROCEDURE is_even(a):
+# {
+#  IF(a % 2 == 0)
+#  {
+#   RETURN("Even")
+#   DISPLAY("Even")
+#  }
+#  IF(a % 2 == 1)
+#  {
+#   RETURN("Odd")
+#   DISPLAY("Odd")
+#  }
+# }
+
+# is_even(a)
+
+def is_even(a):
+    if a % 2 == 0:
+        return "Even"
+        print("Even")
+    if a % 2 == 1:
+        return "Odd"
+        print("Odd")
+
+is_even(a)
+
+# Challenge 4:
+
+# PROCEDURE analyze_word(word):
+# {
+#  IF(type(word) == "string")
+#  {
+#  vowelCount <- 0
+#  consonantCount <- 0
+#  vowels <- ["a", "e", "i", "o", "u"]
+#  FOR letter IN word
+#  {
+#   IF(letter in vowels)
+#   {
+#    vowelCount += 1
+#   }
+#   ELSE
+#   {
+#    consonantCount += 1
+#   }
+#  }
+#  RETURN(f"There are {vowelCount} vowels and {consonantCount} consonants")
+#  }
+# }
+
